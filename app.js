ea238002c9c7d7039870aeac5cd2f46b292afd32
Pratik Yadav <162031829+pratikAD-2003@users.noreply.github.com>
2025-07-24 18:44:54 +0530

    remove tracking files and urls

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/SamsaraBackend")
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Mongo connected"))
     .catch((err) => console.log("Error connecting to MongoDB:", err));
 
