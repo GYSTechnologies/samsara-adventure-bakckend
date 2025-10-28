2ed535fa0693035db4389d25afa702944e29b48d
Pratik Yadav <162031829+pratikAD-2003@users.noreply.github.com>
2025-10-28 18:34:04 +0530

    changes 2

--- config/email.js
+++ config/email.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: false, // Use STARTTLS
+  secure: true, // Use STARTTLS
   requireTLS: true,
   auth: {
     user: process.env.EMAIL,
